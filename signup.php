4f4038c609126e0b449ee5e9315ce1a9149418cd
Antony92in <56954846+Antony92in@users.noreply.github.com>
2020-02-25 11:12:51 +0300

    Класс для регистрации пользователей

---
+++ signup.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once "connect.php";
+
+
+class Signup //класс регистрации пользователей в БД
+{
+	
+	public $errors = array();
+
+	
+
+	public function check()
+	{
+		$user = R::findone('users', 'login = ?', [ $_POST['login']]);
+
+		$email = R::findone('users', 'email = ?', [ $_POST['email']]);
+
+		if (strlen($_POST['password'] < 5)) {
+			
+			$errors[] = 'Пароль должен быть не меннее 5 символов';
+		}
+
+		if ($_POST['login'] == $user['login']) {
+
+			$errors[] = 'Этот логин занят';
+			
+		}
+
+		if ($_POST['email'] == $email['email']) {
+			
+			$errors[] = 'Этот email занят';
+		}
+
+		
+		if (empty($_POST['email'])) {
+			
+			$errors[] = 'Введите email';
+		}
+
+		if (!empty($errors)) {
+		return array_shift($errors);
+	}
+	else
+	{ 
+		$user = R::dispense('users');
+
+		$user->login = $_POST['login'];
+
+		$user->email = $_POST['email'];
+
+		$user->password = password_hash( $_POST['password'] , PASSWORD_DEFAULT);
+
+		R::store( $user );
+
+		return 'OK';
+      }
+	}
+
+
+
+   
+}
+
+echo Signup::check();
+
+
